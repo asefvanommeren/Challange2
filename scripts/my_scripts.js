280aef93d5d03d08da1a5ee7af7e9779f08b83e4
asefvanommeren <79085391+asefvanommeren@users.noreply.github.com>
2021-03-14 20:26:25 +0100

    JS Voorbeelden Les added
    Oefeningen uit lesweek 4 - JS

---
+++ scripts/my_scripts.js
@@ -0,0 +1,54 @@
+var today = new Date();
+
+// document.getElementById('clock').innerHTML = today.toString();
+
+//document.getElementById('clock').innerHTML = today.getDate();
+//document.getElementById('clock').innerHTML = today.getMonth(); // Januari is maand 0
+//document.getElementById('clock').innerHTML = today.getYear(); // Deprecated - niet gebruiken.
+//document.getElementById('clock').innerHTML = today.getFullYear();
+//document.getElementById('clock').innerHTML = today.getDay(); // Zondag is dag 0
+
+// document.getElementById('clock').innerHTML = today.getHours();
+// document.getElementById('clock').innerHTML = today.getMinutes();
+// document.getElementById('clock').innerHTML = today.getSeconds();
+//document.getElementById('clock').innerHTML = today.getMilliseconds();
+
+// -----------------
+// var seconds = today.getSeconds ();
+//   if (seconds < 10) {
+//       seconds = '0' + seconds;
+//   }
+//
+// var minutes = today.getMinutes();
+//   if (minutes < 10) {
+//       minutes = '0' + minutes;
+//     }
+//document.getElementById('clock').innerHTML = today.getHours() + ':' + minutes + ':' + seconds;
+// -----------------
+
+// -----------------
+// var seconds = addLeadingZero(today.getSeconds());
+// var minutes = addLeadingZero(today.getMinutes());
+//
+// document.getElementById('clock').innerHTML = today.getHours() + ':' + minutes + ':' + seconds;
+//
+// function addLeadingZero(number) {
+//   if (number < 10) {
+//     return '0' + number;
+//   }
+//   return number;
+// }
+// -----------------
+
+// -----------------
+
+// -----------------
+
+
+//document.getElementById('clock').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
+
+//var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+//document.getElementById('clock').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
+
+
+//document.getElementById('clock').innerHTML = today.getTime();
